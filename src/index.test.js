78774885bf539545b2d8ae3d1f39072239361fb4
muhkham <muhkham92@gmail.com>
2020-10-28 09:42:31 +0200

    testing

--- src/index.test.js
+++ src/index.test.js
@@ -1,35 +1,43 @@
 // Adding Tests:
 test("Submitting a new task adds it to the list", t => {
-  Adding('a')
-  console.log(text.value);
-  t.equal(Adding(text.value),'Drink Coffee')
+  let expected = 'Drink Coffee';
+  let result = ToDoList[0].text;
+  t.equal(result,expected)
 });
 
-
-// test("Submitting a new task adds it to the list", t => {
-//   t.equal(Adding(Drink Coffee),'Drink Coffee')
-// });
-
+// // test("Submitting a new task adds it to the list", t => {
+// //   t.equal(Adding(Drink Coffee),'Drink Coffee')
+// // });
 
 
 
 
-// // Checking Tests:
-// test("Checking an entry marks it as complete", t => {
-//     t.equal(checkingk(Drink Coffee),'1')
-//   });
 
-//   test("Toggling the filter hides completed tasks from the list", t => {
-//       t.equal(Adding(Drink Coffee),'Drink Coffee')
+// Checking Tests:
+test("Checking an entry marks it as complete", t => {
+  let expected = '1';
+  let result = ToDoList[0].IsChecked;
+    t.equal(result,expected)
+  });
+
+//   test("uncheck the entry", t => {
+//       t.equal(Checking(Drink Coffee),'')
 //     });
+
 
 
 
 
 // // Deleting Tests:
 // test("Deleting an entry removes it from the list", t => {
-//     t.equal(Deleting(Drink Coffee),'')
+//     let expected = null;
+//     let result = Deleting('Drink Coffee');
+//     console.log(result);
+//     t.equal(result,expected);
 //   });
+
+
+
 
 
 
